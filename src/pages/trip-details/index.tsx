564ec181d3bfc35e40a821c5da7a2ec7ba16eaef
reciokaue <kaue.recio2@gmail.com>
2024-07-22 17:03:05 -0300

    activity modal

--- src/pages/trip-details/index.tsx
+++ src/pages/trip-details/index.tsx
@@ -2,14 +2,23 @@ import {
   Calendar,
   CircleCheck,
   CircleDashed,
+  Clock,
   Link2,
   MapPin,
   Plus,
   Settings2,
+  Tag,
   UserCog,
+  X,
 } from 'lucide-react'
+import { useState } from 'react'
 
 export function TripDetailsPage() {
+  const [activityModal, setActivityModal] = useState(false)
+
+  const openActivivityModal = () => setActivityModal(true)
+  const closeActivivityModal = () => setActivityModal(false)
+
   return (
     <div className="mx-auto flex max-w-6xl flex-col space-y-8 px-6 py-10">
       <div className="flex h-16 items-center justify-between rounded-xl bg-zinc-900 px-4 shadow-shape">
@@ -34,7 +43,10 @@ export function TripDetailsPage() {
         <div className="flex-1 space-y-6">
           <header className="flex items-center justify-between">
             <h2 className="text-3xl font-semibold">Atividades</h2>
-            <button className="flex items-center gap-2 rounded-lg bg-lime-300 px-5 py-2 font-medium text-lime-950 hover:bg-lime-400">
+            <button
+              onClick={openActivivityModal}
+              className="flex items-center gap-2 rounded-lg bg-lime-300 px-5 py-2 font-medium text-lime-950 hover:bg-lime-400"
+            >
               <Plus className="size-5" />
               Cadastrar atividade
             </button>
@@ -158,6 +170,48 @@ export function TripDetailsPage() {
           </aside>
         </div>
       </main>
+
+      {activityModal && (
+        <div className="fixed inset-0 flex items-center justify-center bg-black/60">
+          <div className="w-[640px] space-y-5 rounded-xl bg-zinc-900 px-6 py-5 shadow-shape">
+            <div className="flex items-center justify-between">
+              <h2 className="text-lg font-semibold">Cadastrar atividade</h2>
+              <button onClick={closeActivivityModal}>
+                <X className="size-5 text-zinc-400" />
+              </button>
+            </div>
+            <p className="text-sm text-zinc-400">
+              Todos convidados podem visualizar as atividades.
+            </p>
+            <form>
+              <div className="flex flex-col gap-2">
+                <div className="flex items-center gap-2 rounded-lg border border-zinc-800 bg-zinc-950 p-4">
+                  <Tag className="ml-2 size-5 text-zinc-400" />
+                  <input
+                    className="text-large flex-1 bg-transparent placeholder-zinc-400 outline-none"
+                    placeholder="Qual a atividade?"
+                  />
+                </div>
+                <div className="flex flex-1 items-center gap-2 rounded-lg border border-zinc-800 bg-zinc-950 p-4">
+                  <Calendar className="ml-2 size-5 text-zinc-400" />
+                  <input
+                    className="text-large flex-1 bg-transparent placeholder-zinc-400 outline-none"
+                    placeholder="Data e hora"
+                    type="datetime-local"
+                    name="occurs_at"
+                  />
+                </div>
+              </div>
+              <button
+                type="submit"
+                className="mt-3 flex h-11 w-full items-center justify-center gap-2 rounded-lg bg-lime-300 px-5 py-2 font-medium text-lime-950 hover:bg-lime-400"
+              >
+                Salvar atividade
+              </button>
+            </form>
+          </div>
+        </div>
+      )}
     </div>
   )
 }
